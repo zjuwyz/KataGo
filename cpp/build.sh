2ade1b855b00a1ea21b08dc61e12a3abfa7db1cd
wang yize <wyz@wyz>
2025-10-25 15:31:01 +0800

    modify  build.sh to use host env

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,3 +1,4 @@
 #/bin/bash
-sudo rm CMakeCache.txt
-docker run -it --rm -v /home/wangyize/.katago:/root/.katago --gpus all katago-dev bash -c "cd /root/.katago/KataGo/cpp && cmake . -DUSE_BACKEND=TensorRT -DUSE_CACHE_TENSORRT_PLAN=1 -DUSE_TENSORRT_CUDA_GRAPH=1 -DTENSORRT_ROOT_DIR=/TensorRT && make -j8"
+#sudo rm CMakeCache.txt
+#docker run -it --rm -v /home/wangyize/.katago:/root/.katago --gpus all katago-dev bash -c "cd /root/.katago/KataGo/cpp && cmake . -DUSE_BACKEND=TensorRT -DUSE_CACHE_TENSORRT_PLAN=1 -DUSE_TENSORRT_CUDA_GRAPH=1 -DTENSORRT_ROOT_DIR=/TensorRT && make -j8"
+cd /home/wangyize/.katago/KataGo/cpp && cmake . -DUSE_BACKEND=TensorRT -DUSE_CACHE_TENSORRT_PLAN=1 -DUSE_TENSORRT_CUDA_GRAPH=1 -DTENSORRT_ROOT_DIR=/home/wangyize/3rdparty/TensorRT-10.13.3.9 && make -j
